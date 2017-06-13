be714a847cdcbea54cec4a90589473cd133e04f2
Deepam Patel <Deepam1803@yahoo.in>
2017-06-13 10:33:43 +0530

    cahnges

--- app/src/main/java/com/example/deepampatel/androidactivityrecognition/MainActivity.java
+++ app/src/main/java/com/example/deepampatel/androidactivityrecognition/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.deepampatel.androidactivityrecognition;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks{
+
+    private GoogleApiClient googleApiClient;
+    private TextView statusText;
+    private Button requestUpdateButton;
+    private Button removeUpdateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
