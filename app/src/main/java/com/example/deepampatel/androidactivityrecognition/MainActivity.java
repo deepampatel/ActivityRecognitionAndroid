bf9f4ea9f750f4aa5f4eda6559c426832e425cc9
Deepam Patel <Deepam1803@yahoo.in>
2017-06-13 10:52:48 +0530

    receiver class

--- app/src/main/java/com/example/deepampatel/androidactivityrecognition/MainActivity.java
+++ app/src/main/java/com/example/deepampatel/androidactivityrecognition/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.deepampatel.androidactivityrecognition;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.solver.widgets.ConstraintAnchor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,17 +14,35 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener,GoogleApiClient.ConnectionCallbacks{
 
-    private GoogleApiClient googleApiClient;
+    protected GoogleApiClient googleApiClient;
     private TextView statusText;
     private Button requestUpdateButton;
     private Button removeUpdateButton;
+    protected ActivityDetectionBroadcastReceiver broadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestUpdateButton=(Button)findViewById(R.id.request_activity_updates_button);
+        removeUpdateButton=(Button)findViewById(R.id.remove_activity_updates_button);
+        statusText=(TextView)findViewById(R.id.detectedActivities);
+        broadcastReceiver=new ActivityDetectionBroadcastReceiver();
+        buildGoogleApiClient();
+    }
+
+   protected synchronized void buildGoogleApiClient() {
+    googleApiClient=new GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(ActivityRecognition.API)
+            .build();
     }
 
     @Override
@@ -35,5 +58,40 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+    public String getActivityString(int detectedActivityType) {
+        Resources resources = this.getResources();
+        switch(detectedActivityType) {
+            case DetectedActivity.IN_VEHICLE:
+                return resources.getString(R.string.in_vehicle);
+            case DetectedActivity.ON_BICYCLE:
+                return resources.getString(R.string.on_bicycle);
+            case DetectedActivity.ON_FOOT:
+                return resources.getString(R.string.on_foot);
+            case DetectedActivity.RUNNING:
+                return resources.getString(R.string.running);
+            case DetectedActivity.STILL:
+                return resources.getString(R.string.still);
+            case DetectedActivity.TILTING:
+                return resources.getString(R.string.tilting);
+            case DetectedActivity.UNKNOWN:
+                return resources.getString(R.string.unknown);
+            case DetectedActivity.WALKING:
+                return resources.getString(R.string.walking);
+            default:
+                return resources.getString(R.string.unidentifiable_activity, detectedActivityType);
+        }
+
+    public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ArrayList<DetectedActivity>updatedActivities=intent.getParcelableArrayListExtra(Constants.ACTIVITY_EXTRA);
+            String strStatus="";
+            for(DetectedActivity thisActivity:updatedActivities){
+                strStatus+=getActivityString(thisActivity.getType()+thisActivity.getConfidence()+"%\n";)
+            }
+            statusText.setText(strStatus);
+        }
     }
 }
